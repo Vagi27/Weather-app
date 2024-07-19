3ccfd68af09a3c022471bf9b04008882563eb9f6
Vagish <Vagishmlk@gmail.com>
2024-07-19 17:20:19 +0530

    used added APIs to fetch weather for searched request. facing a problem where prefix requests are throwing runtime errors

--- src/App.js
+++ src/App.js
@@ -1,19 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-import Search from './components/Search';
-import CurrentWeather from './components/CurrentWeather';
-import WeeklyWeather from './components/WeeklyWeather';
+import "./App.css";
+import Search from "./components/Search";
+import CurrentWeather from "./components/CurrentWeather";
+import WeeklyWeather from "./components/WeeklyWeather";
+import { useEffect, useState } from "react";
+import {
+  WEATHER_API_URL,
+  API_KEY,
+  GEO_API_OPTIONS,
+  GEO_API_URL,
+} from "./constants";
 
 function App() {
+  const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      if (location) {
+        try {
+          async function getWeather() {
+            const weatherResponse = await fetch(
+              WEATHER_API_URL + location + API_KEY
+            );
+            const geoResponse = await fetch(
+              `${GEO_API_URL}?namePrefix=${location}`,
+              GEO_API_OPTIONS
+            );
+            const weatherData = await weatherResponse.json();
+            const geoData = await geoResponse.json();
+
+            console.log(weatherData);
+            console.log(geoData);
+          }
+          getWeather();
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      }
+    }, 600);
+
+    return () => {
+      clearInterval(handler);
+    };
+  }, [location]);
 
   return (
-    <div> 
-      <Search/>
-      <div className='flex'>
-        <CurrentWeather/>
-        <WeeklyWeather/>
-
+    <div className="flex justify-center">
+      <div className="border border-black p-4 mt-10 w-3/5 h-3/5 rounded-md ">
+        <Search
+          location={location}
+          setLocation={(value) => {
+            setLocation(value);
+          }}
+        />
+        <div className="flex justify-between">
+          <CurrentWeather />
+          <WeeklyWeather />
+        </div>
       </div>
+      <h1>Api:{location}</h1>
     </div>
   );
 }

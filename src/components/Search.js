36b6fe1a9146965a1f84a422812964c182dc3b1f
Vagish <Vagishmlk@gmail.com>
2024-07-19 18:14:37 +0530

    made progress with suggestions drop box, used a module 'react-select-async-paginate'

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,36 @@
-// import { useState } from "react";
+import { useEffect, useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { fetchCities } from "../constants";
 
 const Search = ({ location, setLocation }) => {
+  const [searchLocation, setSearchLocation] = useState("");
+
+  const loadOptions = async (inputValue) => {
+    const citiesList = await fetchCities(inputValue);
+
+    return {
+      options: citiesList.data.map((city) => {
+        console.log(city);
+        return {
+          value: `${city.latitude} ${city.longitude}`,
+          label: `${city.name}, ${city.countryCode}`,
+        };
+      }),
+    };
+  };
+  const onChangeHandler = (e) => {
+    setSearchLocation(e.target.value);
+  };
   return (
-    <div className="border border-black rounded-md p-2">
-      <input
-        className="w-4/5 outline-none"
-        placeholder="Enter Location..."
-        onChange={(e) => {
-          setLocation(e.target.value);
-        }}
-      />
-    </div>
+    // <div className="border border-black rounded-md p-2">
+    <AsyncPaginate
+      placeholder="Search for cities"
+      debounceTimeout={600}
+      value={searchLocation}
+      onChange={onChangeHandler}
+      loadOptions={loadOptions}
+    />
+    // </div>
   );
 };
 export default Search;

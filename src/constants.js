2861e0b6ad30e5482d869fc56e26bc5a45242bb6
Vagish <Vagishmlk@gmail.com>
2024-07-19 17:18:04 +0530

    added a new constants file:  which contains weather API + Key && location Suggestion API + key

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const WEATHER_API_URL =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+export const API_KEY = "?key=XETQGN5JBLQZT6XQT4JZLMYA9";
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const GEO_API_OPTIONS = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "fdea0a0472mshd3028b32a972f48p199729jsn7de6cbbf3f62",
+    "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+  },
+};

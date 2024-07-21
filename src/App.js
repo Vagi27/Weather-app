52e6ebf3db4469f50c9c519e8921136cf6ac3a24
Vagish <Vagishmlk@gmail.com>
2024-07-21 18:24:38 +0530

    now handles selected location from <Search.js/> initializes 2 different states for both current and weekly temperatures. ->passes those states as props to current and weekly forecast components

--- src/App.js
+++ src/App.js
@@ -3,61 +3,56 @@ import Search from "./components/Search";
 import CurrentWeather from "./components/CurrentWeather";
 import WeeklyWeather from "./components/WeeklyWeather";
 import { useEffect, useState } from "react";
-import {
-  WEATHER_API_URL,
-  API_KEY,
-  GEO_API_OPTIONS,
-  GEO_API_URL,
-} from "./constants";
+import { WEATHER_API_URL, API_KEY } from "./constants";
 
 function App() {
-  const [location, setLocation] = useState("");
+  // const [location, setLocation] = useState("");
+  const [currentConditions, setCurrentConditions] = useState({});
+  const [WeeklyConditions, setWeeklyConditions] = useState([]);
 
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      if (location) {
-        try {
-          async function getWeather() {
-            const weatherResponse = await fetch(
-              WEATHER_API_URL + location + API_KEY
-            );
-            const geoResponse = await fetch(
-              `${GEO_API_URL}?namePrefix=${location}`,
-              GEO_API_OPTIONS
-            );
-            const weatherData = await weatherResponse.json();
-            const geoData = await geoResponse.json();
+  const onSearchChange = (enteredLocation) => {
+    const [latitude, longitude] = enteredLocation.value.split(" ");
+    console.log(latitude, longitude);
+    try {
+      async function getWeather() {
+        const weatherResponse = await fetch(
+          `${WEATHER_API_URL} ${latitude},${longitude}${API_KEY}`
+        );
 
-            console.log(weatherData);
-            console.log(geoData);
-          }
-          getWeather();
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
+        const weatherData = await weatherResponse.json();
+        setCurrentConditions(weatherData.currentConditions);
+        setWeeklyConditions(weatherData.days);
+
+        console.log(weatherData);
+        // console.log(geoData);
       }
-    }, 600);
+      getWeather();
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-    return () => {
-      clearInterval(handler);
-    };
-  }, [location]);
+  // useEffect(() => {
+  //   const handler = setTimeout(() => {
+  //     if (location) {
+  //     }
+  //   }, 600);
+
+  //   return () => {
+  //     clearInterval(handler);
+  //   };
+  // }, [location]);
 
   return (
     <div className="flex justify-center">
       <div className="border border-black p-4 mt-10 w-3/5 h-3/5 rounded-md ">
-        <Search
-          location={location}
-          setLocation={(value) => {
-            setLocation(value);
-          }}
-        />
+        <Search onSearchChange={onSearchChange} />
         <div className="flex justify-between">
-          <CurrentWeather />
-          <WeeklyWeather />
+          <CurrentWeather currentConditions={currentConditions} />
+          <WeeklyWeather WeeklyConditions={WeeklyConditions} />
         </div>
       </div>
-      <h1>Api:{location}</h1>
+      {/* <h1>Api:{location}</h1> */}
     </div>
   );
 }

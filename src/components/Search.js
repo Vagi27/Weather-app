e7ca2a8a6fa95fdc47e680b8f4e983ecde0c6a13
Vagish <Vagishmlk@gmail.com>
2024-07-21 18:20:08 +0530

    sent data from child through a prop function of belonging to parent

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { fetchCities } from "../constants";
 
-const Search = ({ location, setLocation }) => {
+const Search = ({ onSearchChange }) => {
   const [searchLocation, setSearchLocation] = useState("");
 
   const loadOptions = async (inputValue) => {
@@ -10,7 +10,7 @@ const Search = ({ location, setLocation }) => {
 
     return {
       options: citiesList.data.map((city) => {
-        console.log(city);
+        // console.log(city);
         return {
           value: `${city.latitude} ${city.longitude}`,
           label: `${city.name}, ${city.countryCode}`,
@@ -19,13 +19,15 @@ const Search = ({ location, setLocation }) => {
     };
   };
   const onChangeHandler = (enteredLocation) => {
+    // console.log(enteredLocation);
     setSearchLocation(enteredLocation);
+    onSearchChange(enteredLocation);
   };
   return (
     // <div className="border border-black rounded-md p-2">
     <AsyncPaginate
       placeholder="Search for cities"
-      debounceTimeout={600}
+      debounceTimeout={300}
       value={searchLocation}
       onChange={onChangeHandler}
       loadOptions={loadOptions}

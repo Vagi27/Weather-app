5ac60c17d2bc924d9eede566fb2bead28891f9f0
Vagish <Vagishmlk@gmail.com>
2024-07-22 18:50:01 +0530

    added a function for calculating weekDay from date, a function to convert F to C, and displayed all details

--- src/components/WeekDay.js
+++ src/components/WeekDay.js
@@ -1,20 +1,35 @@
 import React from "react";
 
-const WeekDay = (day) => {
-    console.log(day);
+const WeekDay = ({ day }) => {
+  const farenToCelcius = (farenheit) => {
+    return (((farenheit - 32) * 5) / 9).toFixed(1);
+  };
+  //   console.log(day);
+  const condition = day?.conditions.split(",")[0];
+
+  const dayOfWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ][new Date(day.datetime).getDay()];
+
   return (
-    <div>
-      <div>
-        <div> Day</div>
-        <div> </div>
+    <div className="flex flex-auto m-1 border border-black rounded-md">
+      <div className="p-2 m-1 w-48 flex-auto text-start">
+        <div> {dayOfWeek}</div>
+        <div className="text-xs"> {condition}</div>
       </div>
-      <div>
-        <div></div>
-        <div></div>
+      <div className="p-2 m-1 w-24 flex-auto text-sm">
+        <div>{farenToCelcius(day?.temp)}°C</div>
+        <div>{day?.cloudcover}%</div>
       </div>
-      <div>
-        <div></div>
-        <div></div>
+      <div className="p-2 m-1 w-24 flex-auto text-sm">
+        <div>{day?.windspeed} km/s</div>
+        <div>{day?.humidity}%</div>
       </div>
     </div>
   );

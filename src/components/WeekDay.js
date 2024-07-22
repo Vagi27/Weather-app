556877b73aa2b914fd472941ee0ec5c1b7a724fd
Vagish <Vagishmlk@gmail.com>
2024-07-22 16:55:30 +0530

    created a new Component which will display weekly weather by the day

---
+++ src/components/WeekDay.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const WeekDay = (day) => {
+    console.log(day);
+  return (
+    <div>
+      <div>
+        <div> Day</div>
+        <div> </div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default WeekDay;

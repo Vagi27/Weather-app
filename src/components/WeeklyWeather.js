b716a470835b51b12b0500cd5bc2c25920e2df04
Vagish <Vagishmlk@gmail.com>
2024-07-22 16:54:10 +0530

    maping array of forecasted weather and sending details through prop into a helper component which will show the details by the day

--- src/components/WeeklyWeather.js
+++ src/components/WeeklyWeather.js
@@ -1,10 +1,18 @@
+import WeekDay from "./WeekDay";
+
 const WeeklyWeather = ({ WeeklyConditions }) => {
-  console.log(WeeklyConditions[0].datetime);
+  console.log(WeeklyConditions[0]?.datetime);
+  let key=0;
   return (
     <>
       <div>
         weekly weather:
-        <p>{WeeklyConditions[0].datetime}</p>
+        {/* <p>{WeeklyConditions[0]?.datetime}</p> */}
+        {WeeklyConditions.filter((item, index) => index < 7).map(
+          (forecastDay,index) => {
+            return <WeekDay day={forecastDay} key={key++} />;
+          }
+        )}
       </div>
     </>
   );

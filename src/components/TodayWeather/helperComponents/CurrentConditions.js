f802e64530c877bc44c1297857c91d6da1346553
Vagish <Vagishmlk@gmail.com>
2024-07-23 18:01:00 +0530

    New component along with data

---
+++ src/components/TodayWeather/helperComponents/CurrentConditions.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { farenToCelcius } from "../../../Utility/utility";
+
+const CurrentConditions = ({ todayConditions, location }) => {
+  const { datetime, temp, conditions, icon } = todayConditions;
+  const expected = conditions.split(",")[0];
+  // console.log(location);
+
+  return (
+    <div className="mb-6">
+      <p className="text-center text-sm font-bold">CURRENT WEATHER</p>
+      <div className="flex mt-4 ">
+        <div className="p-2 ">
+          <p>{location}</p>
+          <p> {datetime}</p>
+        </div>
+
+        <div className="p-2">
+          <p> {farenToCelcius(temp)}°C</p>
+          <p>{expected}</p>
+        </div>
+        <div className=" grid p-2  place-items-center">
+          <p>{icon} icon</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CurrentConditions;

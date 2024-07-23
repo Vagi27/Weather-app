5ea99e402eb6c05215c34b5a8f77e59cdf51cfae
Vagish <Vagishmlk@gmail.com>
2024-07-23 18:02:21 +0530

    component which shall show nest few hours of forecast

---
+++ src/components/TodayWeather/helperComponents/HourlyForecast.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HourlyForecast = () => {
+  return (
+    <div >
+      <p className="text-center text-sm font-bold">HourlyForecast </p>
+      <div className="mt-4">
+
+      </div>
+    </div>
+  );
+};
+
+export default HourlyForecast;

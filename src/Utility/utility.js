7882bfdf2928162a1ce365ae7e153b71f5ef78ed
Vagish <Vagishmlk@gmail.com>
2024-07-26 14:40:18 +0530

    implemented a utility function for getting forecasted weather from list of forecasted hours

--- src/Utility/utility.js
+++ src/Utility/utility.js
@@ -1,3 +1,14 @@
 export const farenToCelcius = (farenheit) => {
   return (((farenheit - 32) * 5) / 9).toFixed(1);
 };
+
+export const getTodayWeatherForecast=(hours)=>{
+  let dateNow=new Date();
+  console.log(dateNow)
+  let timeNow=Math.floor(dateNow.getTime()/1000);
+  console.log(timeNow);
+
+  return hours.filter((hour)=>{
+    return hour.datetimeEpoch > timeNow;
+  });
+}

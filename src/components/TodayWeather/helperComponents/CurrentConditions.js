8c95075936f3a3fcf19cf54a94104c7c44882455
Vagish <Vagishmlk@gmail.com>
2024-07-26 20:30:16 +0530

    responsive text

--- src/components/TodayWeather/helperComponents/CurrentConditions.js
+++ src/components/TodayWeather/helperComponents/CurrentConditions.js
@@ -9,15 +9,15 @@ const CurrentConditions = ({ todayConditions, location }) => {
   return (
     <div className="mb-6">
       <p className="text-center text-sm font-bold">CURRENT WEATHER</p>
-      <div className="flex mt-4   font-semibold justify-around">
+      <div className="flex mt-4  text-xs sm:text-base font-semibold justify-around">
         <div className="p-2 ">
           <p>{location}</p>
-          <p> {datetime}</p>
+          <p className=" text-xs"> {datetime}</p>
         </div>
 
         <div className="p-2">
           <p> {farenToCelcius(temp)}°C</p>
-          <p>{expected}</p>
+          <p className=" text-xs" >{expected}</p>
         </div>
         <div className=" grid p-2  place-items-center">
           <p>{icon} icon</p>

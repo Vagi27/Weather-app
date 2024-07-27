8daa809115288b193c91fa1be42b6dc0afa2c02b
Vagish <Vagishmlk@gmail.com>
2024-07-27 17:30:54 +0530

    added total count of available forecasts, added key and filtered forecast as per dateTime rather than index

--- src/components/TodayWeather/helperComponents/HourlyForecast.js
+++ src/components/TodayWeather/helperComponents/HourlyForecast.js
@@ -2,22 +2,29 @@ import React from "react";
 import { farenToCelcius } from "../../../Utility/utility";
 import { getTodayWeatherForecast } from "../../../Utility/utility";
 const HourlyForecast = ({ hours }) => {
-  console.log(hours);
+  // console.log(hours);
+  let key=101;
   const forecastHours = getTodayWeatherForecast(hours);
-  console.log(forecastHours);
+  // console.log(forecastHours);
   return (
     <div className="text-center ">
       <p className=" text-sm font-bold">TODAY'S FORECAST </p>
-      <p className="text-xs sm:text-sm">available forecasts</p>
+      <p className="text-xs font-semibold">
+        {(forecastHours.length / 3).toFixed(0)}-Available Forecasts
+      </p>
 
       <div className="flex justify-evenly mt-4 font-semibold text-xs sm:text-sm">
         {forecastHours
-          .filter((item,index) => index % 3 === 0)
-          .map((item, index) => {
+          .filter((item, index) => item.datetime.substring(0, 2) % 3 === 0)
+          .map((item) => {
             return (
-              <div className="m-2 p-2">
+              <div className="m-2 p-2" key={key++}>
                 <p>{item.datetime.substring(0, 5)}</p>
-                <img className=" text-xs sm:text-sm"src={item.icon} alt={item.conditions}></img>
+                <img
+                  className=" text-xs sm:text-sm"
+                  src={item.icon}
+                  alt={item.conditions}
+                ></img>
                 <p>{farenToCelcius(item.temp)} °C</p>
               </div>
             );

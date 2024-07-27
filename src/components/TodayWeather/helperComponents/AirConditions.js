f36f80279371df03447cc8a81c36e4c9760889df
Vagish <Vagishmlk@gmail.com>
2024-07-27 17:34:33 +0530

    imported images, applied and sized also fixed DOM

--- src/components/TodayWeather/helperComponents/AirConditions.js
+++ src/components/TodayWeather/helperComponents/AirConditions.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { farenToCelcius } from "../../../Utility/utility";
 
-const AirConditions = ({conditions}) => {
+import temperatureIcon from "../../../assets/temperature.png";
+import cloudIcon from "../../../assets/cloud.png";
+import windIcon from "../../../assets/wind.png";
+import humidityIcon from "../../../assets/humidity.png";
+
+const AirConditions = ({ conditions }) => {
   // console.log(conditions);
   const { feelslike, windspeed, cloudcover, humidity } = conditions;
   return (
     <div className="mb-6">
       <p className="text-center text-base font-bold">AIR CONDITIONS </p>
-      <div className=" mt-4  font-semibold sm:text-base text-xs text-center grid grid-cols-4 gap-2 justify-evenly">
-        <div>
+      <div className=" mt-4 items-center font-semibold sm:text-base text-xs text-center grid grid-cols-4 gap-2 justify-evenly">
+        <div className="flex flex-col  items-center">
+          <img className=" w-4" src={temperatureIcon} alt=""></img>
           <p>Feels Like</p>
           <p>{farenToCelcius(feelslike)}°C</p>
         </div>
-        <div>
+        <div className="flex flex-col  items-center">
+          <img className="w-4 " src={windIcon} alt=""></img>
           <p>Wind</p>
           <p>{windspeed}Km/h</p>
         </div>
-        <div>
+        <div className="flex flex-col  items-center">
+          <img className=" w-4" src={cloudIcon} alt=""></img>
           <p>Clouds</p>
           <p>{cloudcover} %</p>
         </div>
-        <div>
+        <div className="flex flex-col  items-center">
+          <img className=" w-4" src={humidityIcon} alt=""></img>
           <p>Humidity</p>
           <p>{humidity} %</p>
         </div>

9c0ec2da7b5edf3a1ad1e4882b63c6ac40188ef4
Vagish <Vagishmlk@gmail.com>
2024-07-29 21:47:52 +0530

    caching loaded city options using local storage, refactored load options as a seperate function

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,27 +2,40 @@ import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { fetchCities } from "../constants";
 
+const loadOptions = async (inputValue) => {
+  console.log(inputValue);
+  let cacheKey = `citiesList-${inputValue}`;
+  const cachedCityList = localStorage.getItem(cacheKey);
+
+  if (cachedCityList) {
+    return {
+      options: JSON.parse(cachedCityList),
+    };
+  }
+
+  const citiesList = await fetchCities(inputValue);
+
+  const options = citiesList.data.map((city) => {
+    // console.log(city);
+    return {
+      value: `${city.latitude} ${city.longitude}`,
+      label: `${city.name}, ${city.countryCode}`,
+    };
+  });
+
+  localStorage.setItem(cacheKey, JSON.stringify(options));
+
+  return { options };
+};
+
 const Search = ({ onSearchChange }) => {
   const [searchLocation, setSearchLocation] = useState("");
 
-  const loadOptions = async (inputValue) => {
-    const citiesList = await fetchCities(inputValue);
-
-    return {
-      options: citiesList.data.map((city) => {
-        // console.log(city);
-        return {
-          value: `${city.latitude} ${city.longitude}`,
-          label: `${city.name}, ${city.countryCode}`,
-        };
-      }),
-    };
-  };
   const onChangeHandler = (enteredLocation) => {
-    // console.log(enteredLocation);
     setSearchLocation(enteredLocation);
     onSearchChange(enteredLocation);
   };
+
   return (
     <AsyncPaginate
       placeholder="Search for cities"

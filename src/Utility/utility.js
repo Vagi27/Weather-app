c0d61731b9e3e4c06ca8944a77f02b5858db8985
Vagish <Vagishmlk@gmail.com>
2024-07-29 21:48:10 +0530

    spacing

--- src/Utility/utility.js
+++ src/Utility/utility.js
@@ -2,13 +2,13 @@ export const farenToCelcius = (farenheit) => {
   return (((farenheit - 32) * 5) / 9).toFixed(1);
 };
 
-export const getTodayWeatherForecast=(hours)=>{
-  let dateNow=new Date();
+export const getTodayWeatherForecast = (hours) => {
+  let dateNow = new Date();
   // console.log(dateNow)
-  let timeNow=Math.floor(dateNow.getTime()/1000);
+  let timeNow = Math.floor(dateNow.getTime() / 1000);
   // console.log(timeNow);
 
-  return hours.filter((hour)=>{
+  return hours.filter((hour) => {
     return hour.datetimeEpoch > timeNow;
   });
-}
+};

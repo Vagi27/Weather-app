df46181c110fc045d9a404ae90c7ca1d2218b708
Vagish <Vagishmlk@gmail.com>
2024-07-30 14:25:58 +0530

    background gradient for each weekday

--- src/components/WeekDay.js
+++ src/components/WeekDay.js
@@ -19,25 +19,31 @@ const WeekDay = ({ day }) => {
   ][new Date(day.datetime).getDay()];
 
   return (
-    <div className="flex  m-1 border border-black rounded-md justify-between  text-xs sm:text-sm lg:text-base ">
-      <div className="p-1 m-1 basis-1/3 text-start">
+    <div
+      className="flex  m-1  rounded-md justify-between  text-xs sm:text-sm lg:text-base  "
+      style={{
+        background:
+          "linear-gradient(0deg, rgba(255, 255, 255, 0.05) 0%, rgba(171, 203, 222, 0.05) 100%)",
+      }}
+    >
+      <div className="px-1 m-1 basis-1/3 text-start">
         <p> {dayOfWeek}</p>
         <p className="font-normal italic"> {condition}</p>
         {/* <div> {day?.datetime}</div> */}
       </div>
 
-      <div className="p-1 m-1  basis-1/3 text-center  ">
+      <div className="px-1 m-1  basis-1/3 text-center  ">
         <div className="flex p-1 justify-center">
           <img className="w-4  mr-1 " src={temperatureIcon} alt=""></img>
           <p>{farenToCelcius(day?.temp)} °C</p>
         </div>
-        <div className="flex p-1 justify-center">
+        <div className="flex px-1 justify-center">
           <img className="w-5 mr-1 " src={cloudIcon} alt=""></img>
           <p>{day?.cloudcover} % </p>
         </div>
       </div>
 
-      <div className="p-1 m-1  basis-1/3 text-center  ">
+      <div className="px-1 m-1  basis-1/3 text-center  ">
         <div className="flex p-1 justify-center">
           <img className="w-4  mr-1 " src={windIcon} alt=""></img>
           <p>{day?.windspeed} km/h</p>

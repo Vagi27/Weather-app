c5dbba128ecfbd821c996e80e04a1fcbd8bd46b4
Vagish <Vagishmlk@gmail.com>
2024-07-30 14:27:59 +0530

    removed incorrect class name

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
 
   return (
     <div className="flex justify-center min-h-fit ">
-      <div className="   p-4  mx-0  w-full h-3/5 rounded-md md:mx-8 lg:w-[1100px] lg:mt-10 shadow-sm  shadow-gray-300 sha">
+      <div className="   p-4  mx-0  w-full h-3/5 rounded-md md:mx-8 lg:w-[1100px] lg:mt-10 shadow-sm  shadow-gray-300 ">
         <Search onSearchChange={onSearchChange} />
         {hasData ? (
           <div className="flex flex-col mt-8 pt-4 justify-evenly lg:flex-row">

5ce1ef1d68656ac534e854b59d50e7145847000f
Vagish <Vagishmlk@gmail.com>
2024-07-30 11:58:21 +0530

    refactored load options and fetch data, with trying to set local storage with expiry time

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,30 +2,45 @@ import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { fetchCities } from "../constants";
 
-const loadOptions = async (inputValue) => {
-  console.log(inputValue);
-  let cacheKey = `citiesList-${inputValue}`;
-  const cachedCityList = localStorage.getItem(cacheKey);
-
-  if (cachedCityList) {
-    return {
-      options: JSON.parse(cachedCityList),
-    };
-  }
-
+const fetchAndSetList = async (inputValue, cacheKey) => {
   const citiesList = await fetchCities(inputValue);
 
   const options = citiesList.data.map((city) => {
-    // console.log(city);
     return {
       value: `${city.latitude} ${city.longitude}`,
       label: `${city.name}, ${city.countryCode}`,
     };
   });
 
-  localStorage.setItem(cacheKey, JSON.stringify(options));
+  const expiryTime = Date.now() + 5000; // Cache for 5 seconds for testing
+  localStorage.removeItem(cacheKey);
+  const item = { options: options, expiry: expiryTime };
+  localStorage.setItem(cacheKey, JSON.stringify(item));
 
   return { options };
+};
+
+const loadOptions = async (inputValue) => {
+  const cacheKey = `citiesList-${inputValue}`;
+  const cachedCityList = JSON.parse(localStorage.getItem(cacheKey));
+  console.log(cachedCityList);
+  // const cachedCityExpiry = localStorage.getItem(`${cacheKey}-expiry`);
+
+  const currentTime = Date.now();
+  console.log(cachedCityList);
+
+  // if (cachedCityList && parseInt(cachedCityExpiry, 10) > currentTime) {
+  if (cachedCityList && cachedCityList.expiry > currentTime) {
+    console.log("Accessing local storage");
+    return {
+      options: JSON.parse(cachedCityList.options),
+    };
+  } else {
+    console.log("Cache expired or not found, fetching new data");
+    // localStorage.removeItem(cacheKey);
+    // localStorage.removeItem(`${cacheKey}-expiry`);
+    return await fetchAndSetList(inputValue, cacheKey);
+  }
 };
 
 const Search = ({ onSearchChange }) => {
@@ -46,4 +61,5 @@ const Search = ({ onSearchChange }) => {
     />
   );
 };
+
 export default Search;

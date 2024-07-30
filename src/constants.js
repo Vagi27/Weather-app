6eed39330c28736deac84126f87d9929ebf1c768
Vagish <Vagishmlk@gmail.com>
2024-07-30 11:57:47 +0530

    formatting

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,6 @@ export const WEATHER_API_URL =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 export const API_KEY = "?key=XETQGN5JBLQZT6XQT4JZLMYA9";
 
-
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
 export const GEO_API_OPTIONS = {
   method: "GET",
@@ -12,14 +11,11 @@ export const GEO_API_OPTIONS = {
   },
 };
 
-
-export async function fetchCities(inputValue){
-    
-    const geoResponse = await fetch(
-        `${GEO_API_URL}?namePrefix=${inputValue}`,
-        GEO_API_OPTIONS
-      );
-      const geoData = await geoResponse.json();
-      return geoData;
-
+export async function fetchCities(inputValue) {
+  const geoResponse = await fetch(
+    `${GEO_API_URL}?namePrefix=${inputValue}`,
+    GEO_API_OPTIONS
+  );
+  const geoData = await geoResponse.json();
+  return geoData;
 }

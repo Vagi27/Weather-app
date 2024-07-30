a7c8b6a0e09f9996467b51c2f8ebec601ceb2a55
Vagish <Vagishmlk@gmail.com>
2024-07-30 13:44:49 +0530

    sliced maximum of 4 forecast, removed img tags

--- src/components/TodayWeather/helperComponents/HourlyForecast.js
+++ src/components/TodayWeather/helperComponents/HourlyForecast.js
@@ -3,32 +3,29 @@ import { farenToCelcius } from "../../../Utility/utility";
 import { getTodayWeatherForecast } from "../../../Utility/utility";
 const HourlyForecast = ({ hours }) => {
   // console.log(hours);
-  let key=101;
+  let key = 101;
   const forecastHours = getTodayWeatherForecast(hours);
-  // console.log(forecastHours);
+
+  const filteredForecasts = forecastHours
+    .filter((item, index) => item.datetime.substring(0, 2) % 3 === 0)
+    .slice(0, 4); // Limit to 4 forecasts
   return (
     <div className="text-center ">
       <p className=" text-sm font-bold">TODAY'S FORECAST </p>
       <p className="text-xs font-semibold">
-        {(forecastHours.length / 3).toFixed(0)}-Available Forecasts
+        {filteredForecasts.length}-Available Forecasts
       </p>
 
       <div className="flex justify-evenly mt-4 font-semibold text-xs sm:text-sm">
-        {forecastHours
-          .filter((item, index) => item.datetime.substring(0, 2) % 3 === 0)
-          .map((item) => {
-            return (
-              <div className="m-2 p-2" key={key++}>
-                <p>{item.datetime.substring(0, 5)}</p>
-                <img
-                  className=" text-xs sm:text-sm"
-                  src={item.icon}
-                  alt={item.conditions}
-                ></img>
-                <p>{farenToCelcius(item.temp)} °C</p>
-              </div>
-            );
-          })}
+        {filteredForecasts.map((item, index) => {
+          return (
+            <div className="m-2 p-2" key={key++}>
+              <p>{item.datetime.substring(0, 5)}</p>
+              <p className=" italic font-normal text-xs sm:text-sm">{item.conditions}</p>
+              <p>{farenToCelcius(item.temp)} °C</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
